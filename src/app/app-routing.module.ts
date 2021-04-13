87b3e336ba7c31b9de5b0f97d557a64cc610ec2b
mohitkachhadiya <mohitkachhadiya312@gmail.com>
2021-04-13 22:17:06 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+	    path: '',
+	    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  	},{
+	    path: 'about',
+	    loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule)
+  	},{
+	    path: 'projects',
+	    loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule)
+  	},{
+	    path: 'education',
+	    loadChildren: () => import('./modules/education/education.module').then(m => m.EducationModule)
+  	},{
+	    path: 'testimonials',
+	    loadChildren: () => import('./modules/testimonials/testimonials.module').then(m => m.TestimonialsModule)
+  	},{
+	    path: 'contact',
+	    loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
+  	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './modules/shared/header/header.module';
+import { SidebarModule } from './modules/shared/sidebar/sidebar.module';
+import { FooterModule } from './modules/shared/footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    SidebarModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
